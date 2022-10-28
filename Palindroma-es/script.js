f5ae9372ebb204f6566217538042c72e39532a92
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-28 14:18:38 +0200

    Completato esercizio check parola palindroma

--- Palindroma-es/script.js
+++ Palindroma-es/script.js
@@ -1,3 +1,37 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
+
+/*----------------
+    FUNZIONE
+----------------*/
+function ifPalindroma(userWord){
+    let parola = "";
+    for (let i = userWord.length - 1; i >=  0; i--){
+        parola += userWord[i];
+    }
+    if( userWord.toLowerCase() === parola.toLowerCase()){
+        return true;
+    }
+    return false;
+}
+
+/*----------------
+    MAIN
+----------------*/
+const userWord = prompt("Inserisci una parola per sapere se è una palindroma!");
+if (ifPalindroma(userWord)){
+    console.log(`La parola "${userWord}" è una palindroma`);
+} else {
+    console.log(`La parola "${userWord}" non è una palindroma`);
+}
+
+// Esempi parole palindrome:
+    // Esse.
+    // Osso.
+    // Aerea.
+    // Esose.
+    // Aveva.
+    // Inani.
+    // Ingegni.
+    // Ossesso.
